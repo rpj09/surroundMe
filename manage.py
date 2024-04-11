8b5a09a839252578eefb699b805445d5f935f46c
rpj09 <singhripunjay09@gmail.com>
2024-04-11 17:16:32 +0530

    Changes to be committed: 	new file:   .production 	modified:   manage.py 	new file:   music_controller/production.py 	modified:   music_controller/wsgi.py 	modified:   requirements.txt
    Changes not staged for commit:
    	modified:   db.sqlite3
    	modified:   frontend/src/components/HomePage.js
    	modified:   frontend/src/components/Info.js
    	modified:   frontend/src/index.js
    	modified:   frontend/static/frontend/main.js
    	modified:   frontend/static/frontend/main.js.LICENSE.txt
    	modified:   frontend/webpack.config.js
    	modified:   spotify/models.py

--- manage.py
+++ manage.py
@@ -6,7 +6,9 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'music_controller.settings')
+    # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'music_controller.settings')
+    settings_module = "music_controller.production" if 'WEBSITE_HOSTNAME' in os.environ else 'music_controller.settings'
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- music_controller/wsgi.py
+++ music_controller/wsgi.py
@@ -11,6 +11,11 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'music_controller.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'music_controller.settings')
+
+# Check for the WEBSITE_HOSTNAME environment variable to see if we are running in Azure Ap Service
+# If so, then load the settings from production.py
+settings_module = 'music_controller.production' if 'WEBSITE_HOSTNAME' in os.environ else 'music_controller.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
