39ccbf64b32a927318399f902a7e499b7c0183eb
rpj09 <singhripunjay09@gmail.com>
2024-04-11 18:35:39 +0530

    Changes to be committed: 	modified:   music_controller/settings.py

--- music_controller/settings.py
+++ music_controller/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'whfxmu35o6%2)9z71og9*efq^7++so1%@i-nwekkj2d8&fi=$t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
