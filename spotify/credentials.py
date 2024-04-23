7fd528a7410ad11993413359a50d70c6b7955c6a
rpj09 <singhripunjay09@gmail.com>
2024-04-23 14:02:09 +0530

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   spotify/credentials.py

--- spotify/credentials.py
+++ spotify/credentials.py
@@ -1,3 +1,3 @@
 CLIENT_ID = "824d6d5e43c44be3aeeaff252043a7ff"
 CLIENT_SECRET = "8a4f247adc2b4f118478a0ebdae9d61b"
-REDIRECT_URI = "https://surroundme.azurewebsites.net/spotify/redirect"
+REDIRECT_URI = "https://surroundme.azurewebsites.net/redirect"
